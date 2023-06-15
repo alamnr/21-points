216bce0e0e51bbfa7660fa385c9799da6b254d47
alamnr <alamnr@gmail.com>
2023-06-15 10:01:51 +0600

    Initial version of TwwentyOnePoints generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/jhipster/health/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.jhipster.health.repository;
+
+import com.jhipster.health.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
